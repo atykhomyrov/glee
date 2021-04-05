8dd59f1a14b6e589d18a3072f123be7d30c0de68
atykhomyrov <tihomirov93@gmail.com>
2021-04-05 07:59:37 +0300

    added news section

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,13 @@ $('.top-slider__inner').slick({
   autoplay: true,
   autoplaySpeed: 2000
 })
+$('.partners__list').slick({
+  arrows: false,
+  autoplay: true,
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+})
   mixitup('.products__list')
   mixitup('.new-design__list')
 });
